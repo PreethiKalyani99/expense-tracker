6547f97c304645ab1875f929d66495aa662087f4
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-04-04 14:32:56 +0530

    changed styles according to mobile view and desktop view

--- src/components/SingleTransactionHistory.js
+++ src/components/SingleTransactionHistory.js
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 
 export function SingleTrasactionHistory(props){
+
     const [isHover, setIsHover] = useState(false)
     return(
         <>
             <div className={` mb-2 delete-position`} onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} >
-                <button onClick={() => props.deleteTransaction(props.index)} className={`ms-3 delete-button ${isHover ? 'visible' : 'hidden'}`}>X</button>
+                <button onClick={() => props.deleteTransaction(props.index)} className={`delete-button ${isHover ? 'visible' : 'hidden'}`}>X</button>
                 <div className={props.amount < 0 ? "transaction-container red" : 'transaction-container green'} data-testid="transaction-container">
                     <span className="text">{props.text}</span>
                     <span className="amount">{props.amount}</span>
